965fc588f81dbc8fcac5007991b7dfcd46d3e80a
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-12 10:47:29 -0500

    commit - Calculadora .

--- src/main/java/org/example/soap/Calculadora.java
+++ src/main/java/org/example/soap/Calculadora.java
@@ -4,5 +4,17 @@ import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
 @WebService
-public interface Calculadora {  
+public interface Calculadora {
+
+    @WebMethod
+    double sumar(double numero1, double numero2);
+
+    @WebMethod
+    double restar(double numero1, double numero2);
+
+    @WebMethod
+    double multiplicar(double numero1, double numero2);
+
+    @WebMethod
+    double dividir(double numero1, double numero2);  
 }
