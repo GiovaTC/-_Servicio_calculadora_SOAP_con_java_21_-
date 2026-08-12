bc12d6f092be4f547ab9f92022fac0b26c5a60ab
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-08-12 11:00:04 -0500

    commit - PublicarServicio / .

--- src/main/java/org/example/soap/PublicarServicio.java
+++ src/main/java/org/example/soap/PublicarServicio.java
@@ -1,4 +1,27 @@
 package org.example.soap;
 
+import jakarta.xml.ws.Endpoint;
+
 public class PublicarServicio {
+
+    public static void main(String[] args) {
+
+        String direccion = "http://localhost:8080/calculadora";
+
+        Endpoint.publish(
+                direccion,
+                new CalculadoraImpl()
+        );
+
+        System.out.println("------------------------------------------");
+        System.out.println(" SERVICIO SOAP INICIADO");
+        System.out.println("------------------------------------------");
+        System.out.println("URL:");
+        System.out.println(direccion);
+        System.out.println();
+        System.out.println("WSDL:");
+        System.out.println(direccion + "?wsdl");
+        System.out.println("------------------------------------------");
+        System.out.println("Presiona CTRL + C para detener el servicio.");
+    }   
 }
